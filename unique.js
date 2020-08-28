4fb423f4c65af8b967bed2d95c64da019580f213
Jeff C <50182734+jephrocs@users.noreply.github.com>
2020-08-27 22:19:52 -0700

    Update unique.js

--- unique.js
+++ unique.js
@@ -8,3 +8,26 @@ uniqueInOrder = (iterable) => {
     } return empArr
 
 }
+
+
+
+
+if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={() => handleFinishLoading(setLoadingComplete)}
+      />
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <AppNavigator />
+
+
+      </View>
+    );
+  }
+}
